d7aa6b1c15de5ce349ca4e9671e5176e3446368c
heydi0116 <heydilishernandez@gmail.com>
2022-02-13 21:30:28 -0600

    first commit

---
+++ Tarea2Movil/Tarea2Movil/MainPage.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Tarea2Movil
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        async private void btnGuardar_Clicked(object sender, EventArgs e)
+        {
+            var procs = new Models.Datos
+            {
+                nombre = this.txtNombre.Text,
+                apellido = this.txtApellido.Text,
+                edad = Convert.ToInt32(this.txtEdad.Text),
+                correo = this.txtCorreo.Text
+
+            };
+            var pagina = new Page1();
+            pagina.BindingContext = procs;
+            await Navigation.PushAsync(pagina);
+
+        }
+    }
+}
---
+++ Tarea2Movil/Tarea2Movil/Models/Datos.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Tarea2Movil.Models
+{
+    public class Datos
+    {
+        public String nombre { get; set; }
+        public String apellido { get; set; }
+        public Int32 edad { get; set; }
+        public String correo { get; set; }
+
+    }
+}
